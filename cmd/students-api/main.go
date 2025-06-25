fabb2d59741c21c5b047b8dfbd798673b619c175
Shwetabh vikram <108172880+Vikramshwetabh@users.noreply.github.com>
2025-06-25 09:46:02 +0000

    issue in postman

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
-	//	"github.com/vikramshwetabh/students-api/internal/config"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"githubh.com/vikramshwetabh/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -17,19 +23,38 @@ func main() {
 
 	// Setup router
 	router := http.NewServeMux()
-	router.HandleFunc("Get/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, Students API!"))
-	})
+	router.HandleFunc("POST/api/students", student.New()) // Register the student handler
 
 	// Setup server
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
+	// Handle graceful shutdown
+	slog.Info("Starting server...", slog.String("address", cfg.Addr))
+	// Create a channel to listen for interrupt signals
+	done := make(chan os.Signal, 1)
 
-	fmt.Println("Server is running on", cfg.Addr)
-	err := server.ListenAndServe()
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM) // Catch interrupt signal
+
+	// Start server in a goroutine
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server: ", err)
+		}
+	}()
+	<-done // Wait for a signal to stop the server
+
+	slog.Info("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // Create a context with timeout for graceful shutdown
+	defer cancel()
+
+	err := server.Shutdown(ctx) // Shutdown the server gracefully
 	if err != nil {
-		log.Fatal("Failed to start server: ", err)
+		slog.Error("Failed to shutdown: ", slog.String("error", err.Error()))
+	} else {
+		slog.Info("Server stopped Successfully")
 	}
 }
